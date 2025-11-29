5f200d1998ff30ba197fa29b98c7acb0915934e6
Ayush Sahu <ayushsahu1430@gmail.com>
2025-11-29 19:46:33 +0530

    Learned about package.json, package-lock.json, node_modules, core of parcel, features of parcel, browserslist, how our app works faster

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I am an h1 tag"),
-    React.createElement("h2", {}, "I am an h2 tag"),
+  React.createElement("div", { id: "child", key: "child1" }, [
+    React.createElement("h1", { key: "child1-h1" }, "Hi, I am Ayush Sahu 💪"),
+    React.createElement("h2", { key: "child1-h2" }, "I am an h2 tag"),
   ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am also an h1 tag"),
-    React.createElement("h2", {}, "I am also an h2 tag"),
+  React.createElement("div", { id: "child2", key: "child2" }, [
+    React.createElement("h1", { key: "child2-h1" }, "I am also an h1 tag"),
+    React.createElement("h2", { key: "child2-h2" }, "I am also an h2 tag"),
   ]),
 ]);
 
---
+++ README.md
@@ -0,0 +1,20 @@
+# Namaste React 🚀
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File Watching Algorithm - Written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compression
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagonistics
+- Error Handling
+- HTTPs
+- Tree Shaking - Removes unused code
+- Different Dev andd Prod Bundles
