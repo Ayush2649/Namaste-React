824740c7fe45cbc3584b824a9466d005aeb8cba2
Ayush Sahu <ayushsahu1430@gmail.com>
2025-12-01 18:34:46 +0530

    Learnt about Components and Component Composition

--- App.js
+++ App.js
@@ -1,15 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { jsx } from "react/jsx-runtime";
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World");
+const Title = () => <h1 id="heading">Hello World using JSX</h1>;
 
-console.log(heading);
-
-const jsxHeading = <h1 id="heading">Hello World using JSX</h1>
-
-console.log(jsxHeading);
+const HeadingComponent = () => {
+  return    <div id="container">
+                <Title /> 
+                <h1>Hello from HeadingComponent. Hii!</h1>
+            </div> 
+  
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+root.render(<HeadingComponent />);
