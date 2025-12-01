04131f2b433dc0016f047b0ecdecffebdc1f9d84
Ayush Sahu <ayushsahu1430@gmail.com>
2025-12-01 10:01:11 +0530

    Learned about JSX (JavaScript XML)

--- App.js
+++ App.js
@@ -1,19 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { jsx } from "react/jsx-runtime";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child", key: "child1" }, [
-    React.createElement("h1", { key: "child1-h1" }, "Hi, I am Ayush Sahu 💪"),
-    React.createElement("h2", { key: "child1-h2" }, "I am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2", key: "child2" }, [
-    React.createElement("h1", { key: "child2-h1" }, "I am also an h1 tag"),
-    React.createElement("h2", { key: "child2-h2" }, "I am also an h2 tag"),
-  ]),
-]);
+const heading = React.createElement("h1", { id: "heading" }, "Hello World");
 
-console.log(parent);
+console.log(heading);
+
+const jsxHeading = <h1 id="heading">Hello World using JSX</h1>
+
+console.log(jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(jsxHeading);
