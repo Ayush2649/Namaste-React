cefa22b5a188cdd3db6a05b746dc2df411f08327
Ayush Sahu <ayushsahu1430@gmail.com>
2025-12-01 19:13:34 +0530

    Completed Episode 3

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => <h1 id="heading">Hello World using JSX</h1>;
+const Title = () => <h1>Hello World</h1>;
 
 const HeadingComponent = () => {
-  return    <div id="container">
-                <Title /> 
-                <h1>Hello from HeadingComponent. Hii!</h1>
-            </div> 
-  
+  return (
+    <div id="container">
+      {Title()}
+      <Title />
+      <Title></Title>
+      <h1>Hello from HeadingComponent. Hii!</h1>
+    </div>
+  );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
