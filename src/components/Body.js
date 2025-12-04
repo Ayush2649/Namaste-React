9437de7ef4c95981fc937c0f26940e6c31dfb265
Ayush Sahu <ayushsahu1430@gmail.com>
2025-12-04 12:38:31 +0530

    Implemented the Top Rated Restaurants Button using useState Hook

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,27 @@
 import {resList} from "../utils/mockData.js";
 import RestaurantCard from "./RestaurantCard.js";
+import { useState } from "react";
 
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.avgRatingString > 4.5
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.id} resData={restaurant} />
         ))}
       </div>
