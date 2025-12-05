ea93a4d9864605fd6c4e4c9e2c9476acc3a0ce1f
Ayush Sahu <ayushsahu1430@gmail.com>
2025-12-05 12:12:43 +0530

    Fetched Data from Swiggy's live API aand implemented Shimmer UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,18 +1,41 @@
-import {resList} from "../utils/mockData.js";
 import RestaurantCard from "./RestaurantCard.js";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Shimmer from "./Shimmer.js";
 
 const Body = () => {
-  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+  const [listOfRestaurants, setListOfRestaurants] = useState([]);
 
-  return (
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.2004765&lng=72.8736892&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+
+    console.log(json);
+
+    const onlyValidRestaurants =
+      json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants.filter(
+        (r) => r?.info
+      ) || [];
+
+    setListOfRestaurants(onlyValidRestaurants);
+  };
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
           className="filter-btn"
           onClick={() => {
             const filteredList = listOfRestaurants.filter(
-              (res) => res.avgRatingString > 4.5
+              (res) => res.info?.avgRatingString > 4.5
             );
             setListOfRestaurants(filteredList);
           }}
@@ -21,9 +44,14 @@ const Body = () => {
         </button>
       </div>
       <div className="res-container">
-        {listOfRestaurants.map((restaurant) => (
-          <RestaurantCard key={restaurant.id} resData={restaurant} />
-        ))}
+        {listOfRestaurants
+          .filter((r) => r?.info)
+          .map((restaurant, index) => (
+            <RestaurantCard
+              key={restaurant.info?.id || index}
+              resData={restaurant.info}
+            />
+          ))}
       </div>
     </div>
   );
